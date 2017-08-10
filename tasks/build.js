eb1361c0c4e5cd130f609b8597d832caec9fcb5d
spursy <582214816@qq.com>
2017-08-10 18:22:22 +0800

    add webpack-quickstartdemo code.

---
+++ tasks/build.js
@@ -0,0 +1,51 @@
+// rm (rf,  assestsPath)
+// mkdir(assetsPath)
+// renderConf.entry
+// var compiler = webpack(renderConf)
+// compiler.watch({}, (err, stats))
+require('shelljs/global');
+
+const webpack = require('webpack');
+const fs = require('fs');
+const _ = require('lodash');
+const {resolve} = require('path');
+
+const r = url => resolve(process.cwd(), url)
+
+const webpackConf = require('./webpack.conf');
+const config = require(r('./mina-config'));
+const assetsPath = r('./mina');
+
+rm('-rf', assetsPath)
+mkdir(assetsPath)
+
+var renderConf = webpackConf
+
+renderConf.entry = () => _.reduce(config.json.pages, (en, i) => {
+    en[i] = resolve(process.cwd(), './', `${i}.mina`)
+    return entry
+})
+
+renderConf.entry = entry()
+renderConf.entry.app = config.app
+
+renderConf.output = {
+    path: r('./mina'),
+    filename: '[name].js'
+}
+
+var compiler = webpack(renderConf)
+fs.writeFileSync(r('./mina/app.js'), JSON.stringify(config.json), 'utf-8')
+
+compiler.watch({
+    aggregateTimeout: 300,
+    poll: true
+}, (err, stats) => {
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: true,
+        chunks: true,
+        chunkModules: true
+    }) + '\n\n')
+})
---
+++ tasks/webpack.conf.js
@@ -0,0 +1,98 @@
+const {resolve} = require('path');
+const r = url => resolve(__dirname, url)
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ExtractTestPlugin = require('extract-text-webpack-plugin');
+const extractSass = new ExtractTestPlugin({
+    filename: '[name].wxss'
+})
+
+module.exports = {
+    devtool: false,
+    output: {
+        path: r('./mina'),
+        filename: '[name].js'
+    },
+    resolve: {
+        alias: {
+            utils: r('../utils/util')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: {
+                       ['env', {
+                         modules: false
+                       }]
+                    }
+                }
+            },
+            {
+                test: /\.sass$/,
+                use: extractSass.extract({
+                    use: [
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: {
+                                    plugins: (loader) => [
+                                        require('autoprefixer') ({
+                                            browers: [
+                                                'last 2 versions'
+                                            ]
+                                        })
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                indentedSyntax: true
+                            }
+                        }
+                    ],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.mina$/,
+                loader: 'wechat-mina-loader',
+                options: {
+                    path: r('../'),
+                    dist: './mina'
+                }
+            }
+        ]
+    }
+},
+{
+    plugins: [
+        extractSass,
+        new CopyWebpackPlugin([
+            {
+                from: {
+                    global: 'pages/**/*.json',
+                },
+                to: ''
+            }, 
+            {
+                from: 'static',
+                to: 'static'
+            }
+        ]), 
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            sourcemap: false
+        }),
+        new ProgressBarPlugin()
+    ]
+}
